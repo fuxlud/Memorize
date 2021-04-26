d581129b8cf2807d1c03b84c6bc504727df356f9
Luda <fuxlud@gmail.com>
2021-04-26 12:18:15 +0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Admin on 26/04/2021.
+//  Copyright © 2021 fuxlud. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
